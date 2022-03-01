0d789aad61702e4ba64fc362038cde463156b28a
gopala-kr <gopal.rms4@gmail.com>
2022-03-01 16:38:36 +0000

    Section 1

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Dating_API.Data;
+using Dating_API.Entites;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dating_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+   
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context) {
+
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>>GetUsers() {
+            return await _context.Users.ToListAsync();
+          
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int Id)
+        {
+            return await _context.Users.FindAsync(Id);
+            
+        }
+
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Dating_API.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dating_API.Data
+{
+    public class DataContext : DbContext 
+    {
+ 
+        public DataContext(DbContextOptions options) : base(options) { }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
